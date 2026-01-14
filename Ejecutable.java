1f5ab487f4bf94c5e5cbda686d4977bc28d26490
ManuelCamachoFont <manuelcamachofont@gmail.com>
2026-01-14 01:07:47 +0100

    Creación de ventanas
    Primera versión de creación de interfaz con ventanas

---
+++ Ejecutable.java
@@ -0,0 +1,244 @@
+package es.studium;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejecutable implements ActionListener, WindowListener {
+	
+	Frame vntInicio, vntPruebas, vntCombate, vntCombatientes;
+	
+	Button btnPrbs = new Button("Campo de Pruebas");
+	Button btnAre = new Button("Arena a muerte");
+	Button btnSlct = new Button("Seleccionar");
+	Button btnFight = new Button("¡A pelear!");
+	Button btnExit = new Button("Salir");
+	
+	GridBagConstraints gbc = new GridBagConstraints();
+	
+	Random aleatorio = new Random();
+	int colorR = aleatorio.nextInt(255)+1;
+	int colorG = aleatorio.nextInt(255)+1;
+	int colorB = aleatorio.nextInt(255)+1;
+	
+	private static Arma[] armas = new Arma[7];
+	private static Armadura[] armaduras = new Armadura[7];
+	private static Personaje[] personajes = new Personaje[6];
+	
+	{
+		armas[0] = new Arma("Palo", 1, 10, 20);
+		armas[1]= new Arma("Daga", 10, 30, 100);
+		armas[2] = new Arma("Espada corta", 20, 40, 100);
+		armas[3]= new Arma("Hacha", 30, 80, 100);
+		armas[4] = new Arma("Maza", 20, 50, 100);
+		armas[5] = new Arma("Lanza", 15, 40, 100);
+		armas[6] = new Arma("Espadón", 40, 95, 100);
+		
+		
+		
+		armaduras[0] = new Armadura("Nada", 0, 25);
+		armaduras[1] = new Armadura("Coraza", 35, 100);
+		armaduras[2] = new Armadura("Cota de malla", 40, 100);
+		armaduras[3] = new Armadura("Ropas de cuero", 10, 100);
+		armaduras[4] = new Armadura("Armadura mágica", 50, 100);
+		armaduras[5] = new Armadura("Armadura pesada", 65, 100);
+		armaduras[6] = new Armadura("Armadura Legendaria", 80, 100);
+		
+		
+		personajes[0] = new Personaje("Pepe", "Humano", armas[0], armaduras[6], 10, 10, 100);
+		personajes[1] = new Personaje("Gnome Mates", "Goblin", armas[2], armaduras[2], 20, 20, 75);
+		personajes[2] = new Personaje("Pepe", "Elfo", armas[1], armaduras[4], 10, 30, 120);
+		personajes[3] = new Personaje("Pepe", "Troll", armas[4], armaduras[0], 40, 10, 200);
+		personajes[4] = new Personaje("Enano", "Enano", armas[3], armaduras[5], 20, 10, 140);
+		personajes[5] = new Personaje("Sir Pent", "Reptiliano", armas[5], armaduras[3], 10, 20, 110);
+	}
+	
+	public Ejecutable() {
+		
+		inicio();
+	}
+
+	private void inicio(){
+		
+		vntInicio = new Frame("Inicio");
+		vntInicio.setLayout(new GridBagLayout());
+		
+		gbc.gridx = 0;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		
+		gbc.gridy = 0;
+		vntInicio.add(btnPrbs, gbc);
+		
+		gbc.gridy = 1;
+		vntInicio.add(btnAre, gbc);
+		
+		gbc.gridy = 2;
+		vntInicio.add(btnExit, gbc);
+		
+		vntInicio.setBackground(new Color(colorR, colorG, colorB));
+		vntInicio.setSize(250, 250);
+		vntInicio.setResizable(false);
+		vntInicio.setLocationRelativeTo(null);
+		vntInicio.setVisible(true);
+	}
+
+	private void prbs() {
+		
+		
+	}
+	
+	private void arena() {
+		
+		
+	}
+	
+	
+	private void combate() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		new Ejecutable();
+
+        Scanner teclado = new Scanner(System.in);
+        char salir;
+        
+        do {
+        	System.out.println("CAMPO DE PRUEBAS");
+        	System.out.println("");
+        	
+        	for (int i = 0; i < armas.length; i++) {
+        		String nombrearma = armas[i].getNombre();
+        		String formateada1 = String.format("%-13s", nombrearma);
+        		System.out.println("ARMA: " + formateada1 + " |DAÑO:  " + armas[i].getMindmg() + " ~ " + armas[i].getMaxdmg() + " |DURABILIDAD: " + armas[i].getDurabilidad());
+        		
+        	}
+        	
+        	System.out.println("\nELIGE UN ARMA");
+        	for (int i = 0; i < armas.length; i++) {
+            System.out.println(i + " - " + armas[i].getNombre());
+        	}
+        
+        	int opcionArma = teclado.nextInt();
+          
+        	while (opcionArma < 0 || opcionArma >= armas.length) {
+        		System.out.println("Opción inválida. Elige otra:");
+        		opcionArma = teclado.nextInt();
+        	}
+        	
+        	System.out.println("\nElegiste \"" + armas[opcionArma].getNombre() + "\"");
+        	Arma armaElegida = armas[opcionArma];
+        	System.out.println("");
+        	
+        	for (int i = 0; i < armaduras.length; i++) {
+        		String nombrearmadura = armaduras[i].getNombre();
+        		String formateada2 = String.format("%-20s", nombrearmadura);
+        		System.out.println("ARMADURA: " + formateada2 + " |RESISTENCIA:  " + armaduras[i].getResistencia() + " |DURABILIDAD: " + armaduras[i].getDurabilidad());        		
+        	}
+        	
+        	System.out.println("\nELIGE UNA ARMADURA");
+        	for (int i = 0; i < armaduras.length; i++) {
+            System.out.println(i + " - " + armaduras[i].getNombre());
+        	}
+
+        	int opcionArmadura = teclado.nextInt();
+        
+        	
+        
+        	while (opcionArmadura < 0 || opcionArmadura >= armaduras.length) {
+        		System.out.println("Opción inválida. Elige otra:");
+        		opcionArmadura = teclado.nextInt();
+        	}
+        	
+        	System.out.println("\nElegiste \"" + armaduras[opcionArmadura].getNombre() + "\"");
+        	Armadura armaduraElegida = armaduras[opcionArmadura];
+
+     
+        	System.out.println("\nCOMBATE");
+        	System.out.println("Vas a realizar un ataque con un/una \"" + armaElegida.getNombre() + "\" a un/una \"" + armaduraElegida.getNombre() + "\"." );
+  
+
+        	int dmg = armaElegida.atacar(armaduraElegida);
+        
+        	if (dmg < armaduraElegida.getResistencia()) {
+        		System.out.println("\nEl arma realiza un golpe de " + dmg + " .");
+        		System.out.println("El golpe ha sido resistido.");
+        		System.out.println("Durabilidad arma: " + armaElegida.getDurabilidad());
+        		System.out.println("Durabilidad armadura: " + armaduraElegida.getDurabilidad());
+        	}
+        
+        	else {
+        		System.out.println("\nEl arma realiza un golpe de " + dmg + " .");
+        		System.out.println("El arma ha atravesado la armadura.");
+        		System.out.println("Durabilidad " + armaElegida.getNombre() + " : " + armaElegida.getDurabilidad());
+        		System.out.println("Durabilidad " + armaduraElegida.getNombre() + " : " + armaduraElegida.getDurabilidad());
+        	}
+        	
+        	 System.out.println("\nPulsa '.' para salir a la arena de combate o cualquier otra tecla para continuar en el campo de puebas:");
+        	 salir = teclado.next().charAt(0);
+        	 
+        	    
+        } while (salir != '.');
+        
+        System.out.println("\nARENA DE COMBATE");
+        
+        for (int i = 0; i < personajes.length; i++) {
+    		String nombrepersonaje = personajes[i].getNombre();
+    		String formateada3 = String.format("%-13s", nombrepersonaje);
+    		System.out.println("PERSONAJES: " + formateada3 + " |RAZA:  " + personajes[i].getRaza() + " |ARMA: " + personajes[i].getArma().getNombre() + " |ARMADURA: " + personajes[i].getArmadura().getNombre() + " |FUERZA: " + personajes[i].getFuerza() + " |VELOCIDAD: " + personajes[i].getVelocidad() + " |VIDA: " + personajes[i].getVida() );
+    		
+    	}
+        
+        do {
+        	
+        } while (salir!= '.');
+        
+        teclado.close();
+    }
+
+	@Override
+	public void windowOpened(WindowEvent e){}
+
+	@Override
+	public void windowClosing(WindowEvent e)
+	{
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e){}
+
+	@Override
+	public void windowIconified(WindowEvent e){}
+
+	@Override
+	public void windowDeiconified(WindowEvent e){}
+
+	@Override
+	public void windowActivated(WindowEvent e){}
+
+	@Override
+	public void windowDeactivated(WindowEvent e){}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+		
+	}
+	
+	
+
+}
